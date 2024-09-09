1785b5f88bd44c3c3599ef3fcca4a7994ba33239
checkuu-0216 <corb37@gmail.com>
2024-09-09 17:07:41 +0900

    [REFACTOR] Level1.1 코드개선 퀴즈 Early Return

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -26,14 +26,16 @@ public class AuthService {
 
     @Transactional
     public SignupResponse signup(SignupRequest signupRequest) {
-
-        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
-
-        UserRole userRole = UserRole.of(signupRequest.getUserRole());
+        if (signupRequest.getEmail().isEmpty()) {
+            throw new NullPointerException("이메일을 입력하지 않았습니다.");
+        }
 
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
             throw new InvalidRequestException("이미 존재하는 이메일입니다.");
         }
+        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+
+        UserRole userRole = UserRole.of(signupRequest.getUserRole());
 
         User newUser = new User(
                 signupRequest.getEmail(),
